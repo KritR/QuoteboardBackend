9189f63311492e6489fb0041af37a18115dfc8a9
Krithik Rao <krdevmail@gmail.com>
2018-05-29 21:06:29 -0500

    Added `.formatter.exs` and formatted
    Added the formatter configuration to make sure all necessary files are
    formatted on each run. Ran it to update format of tests and configs.

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :quoteboard,
-  ecto_repos: [Quoteboard.Repo]
+config :quoteboard, ecto_repos: [Quoteboard.Repo]
 
 # Configures the endpoint
 config :quoteboard, QuoteboardWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "fEtzrWN5K8C7uJOYKHBS403iY0klNEXlHoVX5cyIKG3pvjno1nUjiwEegDDx+d4q",
   render_errors: [view: QuoteboardWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: Quoteboard.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Quoteboard.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,11 +22,14 @@ config :logger, :console,
 
 # Configures Guardian
 config :quoteboard, Quoteboard.Guardian,
-  allowed_algos: ["HS512"], # optional
-  verify_module: Guardian.JWT,  # optional
+  # optional
+  allowed_algos: ["HS512"],
+  # optional
+  verify_module: Guardian.JWT,
   issuer: "Quoteboard",
-  ttl: { 30, :days },
-  verify_issuer: true, # optional
+  ttl: {30, :days},
+  # optional
+  verify_issuer: true,
   secret_key: "Q/pRXuJQoZblGk4AIOHhMX0AkzuUpBS91hQVlO06PqrtRd/iAobc3CdBkMPDVYgc"
 
 # Configures Auth Pipeline
@@ -38,4 +39,4 @@ config :quoteboard, Quoteboard.Auth.AuthAccessPipeline,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
